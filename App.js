fd63ef875d0a64132db2b34904863cfd61ef12a8
David <113623882+DavidSvtc@users.noreply.github.com>
2023-03-18 18:02:47 +0200

    .
    .

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
-import { View } from 'react-native';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Gameboard from './components/Gameboard';
 import Scoreboard from './components/Scoreboard';
 import Home from './components/Home';
-import styles from './style/style';
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
